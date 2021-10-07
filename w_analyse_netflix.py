b6214f2cd7e1e0b1d1f1a3044d49796acc64dca1
real-4w <real_4w@protonmail.com>
2021-10-07 17:22:44 +1300

    Profile analysis WIP

--- w_analyse_netflix.py
+++ w_analyse_netflix.py
@@ -1,13 +1,28 @@
 import pandas as pd
-import pathlib, pytz, matplotlib
+import pathlib, pytz, matplotlib, yaml
+#=============================================================================================================
+def ProcessYAML (yaml_file) :
+    '''This function opens the yaml file and returns the data object'''
+    with open(yaml_file) as f:
+        y_data = yaml.load(f, Loader=yaml.FullLoader)
+        debug = y_data['debug']
+        if debug == True : print("YAML file:\n", y_data)
+    return (debug, y_data) 
+def ReturnTotalTimePerProfile(df_netflix):
+    df2 = df_netflix.loc[:, ['Profile Name','Duration']]
+    #df2 = df[['Profile Name','Duration']] #throws a warning
+    #df2 = df.loc[(df['Profile Name'] == "Alex"), ['Profile Name','Duration']]
+    df2['Duration'] = pd.to_timedelta(df2['Duration'])
+    df2 = df2.groupby(['Profile Name'])['Duration'].sum()
+    return (df2)
 
-
-#check the location of the CSV file. I used a data subdirectory, followed by Netflix' directory structure.
-
-#=Main programme ===============================================================================================
+#= Main programme ===============================================================================================
 if __name__ == "__main__":                                      # only run this as standalone script
-    path = pathlib.Path.cwd() / 'data/CONTENT_INTERACTION/ViewingActivity.csv'
+    debug, yaml_data = ProcessYAML('netflix.yaml') 
+    file = yaml_data['view']
+    path = pathlib.Path.cwd() / file
     df = pd.read_csv(path)
+    
     df.shape
     df.head(1)
     #dropping some columns
